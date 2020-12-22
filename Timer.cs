dff1651f56f27893aed8b036e2ef5790b0dd6fe2
Falcn15 <44118568+Falcn15@users.noreply.github.com>
2020-12-22 17:43:52 +0000

    Add files via upload

---
+++ Timer.cs
@@ -0,0 +1,24 @@
+﻿public class Timer
+{
+    public float time { get; private set; }
+
+    public Timer(float time)
+    {
+        SetTime(time);
+    }
+
+    public void SetTime(float time)
+    {
+        this.time = time;
+    }
+
+    public void StepTime(float DeltaTime)
+    {
+        time -= DeltaTime;
+
+        if (time < 0f)
+        {
+            time = 0f;
+        }
+    }
+}
