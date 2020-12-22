083492bd4656d81e01918f392da6612820879253
Falcn15 <44118568+Falcn15@users.noreply.github.com>
2020-12-22 18:02:04 +0000

    Add files via upload

---
+++ TimerBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerBehaviour : MonoBehaviour
+{
+    private Timer timer;
+
+    public bool isActived { get; private set; } = false;
+
+    [SerializeField] float time;
+    [SerializeField] bool ActiveOnStart = true;
+    [SerializeField] UnityEvent OnTimerEnds;
+
+    private void Start()
+    {
+        if (ActiveOnStart)
+        {
+            StartTimer();
+        }
+    }
+
+    private void Update()
+    {
+        if (timer != null)
+        {
+            if (timer.time == 0)
+            {
+                OnTimerEnds?.Invoke();
+            }
+            else
+            {
+                timer.StepTime(Time.deltaTime);
+                //Debug.Log(timer.time);
+            }
+        }
+    }
+
+    public void SetTime(float time)
+    {
+        this.time = time;
+    }
+
+    public void StartTimer()
+    {
+        timer = new Timer(time);
+        isActived = true;
+    }
+
+    public void StartTimer(float time)
+    {
+        SetTime(time);
+        StartTimer();
+    }
+
+    public float GetTime()
+    {
+        return timer.time;
+    }
+}
---
+++ TimerTestScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTestScript : MonoBehaviour
+{
+    TimerBehaviour timerBehaviour;
+    bool hasSentWarning = false;
+
+    private void Start()
+    {
+        timerBehaviour = GetComponent<TimerBehaviour>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("User Pressed Space!");
+            timerBehaviour.StartTimer(10f);
+        }
+
+        if (timerBehaviour.GetTime() < 5f && !hasSentWarning && timerBehaviour.isActived)
+        {
+            hasSentWarning = true;
+            Debug.Log("Timer has 5 seconds left: " + timerBehaviour.GetTime().ToString());
+        }
+    }
+
+}
